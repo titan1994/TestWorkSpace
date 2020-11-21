b1de270badf3ff9b4b199856128e80b8ad01aefe
Atom <kozlov.titan2010@yandex.ru>
2020-11-21 14:16:33 +0300

    1

--- test/test_zep.py
+++ test/test_zep.py
@@ -3,3 +3,19 @@ coefficient = [3, 2, 2]
 
 for i, j in zip(values, coefficient):
     print(i * j)
+
+
+scores = [54,67,48,99,27]
+for i, score in enumerate(scores):
+   print(i, score)
+
+print(enumerate(scores))
+
+
+rg_test = range(1,20)
+
+print(rg_test)
+
+my_crazy_iterator = iter(rg_test)
+print(my_crazy_iterator)
+print(next(my_crazy_iterator))

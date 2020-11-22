04589d22f32cfe5178054a34d1d20d5a11039b94
titan1994 <kozlov.titan2010@yandex.ru>
2020-11-22 21:37:02 +0300

    1

--- my_lib/python_run.py
+++ my_lib/python_run.py
@@ -11,24 +11,40 @@ virtual_env_name = 'venv'
 virtual_lib_catalog_name = 'Lib/site-packages'
 
 
-def lib_install(name_pack):
+def lib_install(name_pack, name_module='pip'):
     """
     Установка пакетов библиотек
 
     Args:
         name_pack: имя пакета
+        name_module: имя модуля pip, pipwin
+    """
+
+    if name_module not in ('pip', 'pip3'):
+        pip_install_pack(name_module, 'pip')
+
+    pip_install_pack(name_pack, name_module)
+
+
+def pip_install_pack(name_pack, name_module='pip'):
+    """
+    Установка пакетов библиотек через указанный модуль
+
+    Args:
+        name_pack: имя пакета
+        name_module: pip, pipwin
     """
 
     python_pass = get_python_pass()
     if virtual_env_name not in python_pass:
         print('Внимание! Не настроено виртуальное оркужение!')
 
-        inst_cmd = 'pip install --upgrade {}'.format(name_pack)
+        inst_cmd = '{} install --upgrade {}'.format(name_module, name_pack)
     else:
         work_space, *python_exe = python_pass.split('{}{}'.format(virtual_env_name, Path('/')))
         path_pack = Path('{}/{}/{}'.format(work_space, virtual_env_name, virtual_lib_catalog_name))
 
-        inst_cmd = 'pip install --upgrade --ignore-installed --target={} {}'.format(path_pack, name_pack)
+        inst_cmd = '{} -m {} install --upgrade --target={} {}'.format(python_pass, name_module, path_pack, name_pack)
 
     print(inst_cmd)
     system(inst_cmd)

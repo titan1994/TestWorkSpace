22d62c9b7f02abf7bc16e863f495763777095bd7
titan1994 <kozlov.titan2010@yandex.ru>
2020-11-22 18:58:23 +0300

    1

---
+++ test/functions/test_funct_intro.py
@@ -0,0 +1,11 @@
+﻿from pprint import pprint
+
+
+def show_abc(a: 'spam', b: (1, 10), c: float) -> int:
+    print(a, b, c)
+
+
+pprint(dir(show_abc.__code__))
+
+pprint(show_abc.__code__.co_varnames)
+pprint(show_abc.__annotations__)
